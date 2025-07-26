dfbdd3c5fa3b4aaf662abcaa392df5d0fb68566e
callagano <31805459+callagano@users.noreply.github.com>
2025-07-26 16:06:18 +0200

    style: Move and restyle expansion panel, slider, and timeframe selector as requested

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Market Melodrama",
+  title: "Fear and Greed index",
   description: "Market sentiment analysis based on multiple indicators",
 };
 
